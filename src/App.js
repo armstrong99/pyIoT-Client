c17ac2f4b2b9612720a322e22e23cd09f9e78e36
Ndukwe Armstrong <armstrong.ndukwe@gmail.com>
2021-10-23 08:35:37 +0100

    done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import io from "socket.io-client";
+import blade from "./blade.png";
+import circ from "./cir.png";
+
+const endPoint = "http://192.168.199.244:8060";
+const IoT = io.connect(`${endPoint}`);
 
 function App() {
+  const [anim, setAnim] = useState("");
+  const [msgX, setMsg] = useState("");
+
+  useEffect(() => {
+    getMsg();
+  }, []);
+
+  const getMsg = () => {
+    IoT.on("iotRecive", (msg) => {
+      vibrate();
+      setMsg(msg);
+    });
+  };
+
+  const vibrate = () => {
+    setAnim("App-logo");
+    window.navigator.vibrate([1000, 500, 1000, 500, 2000]);
+    setTimeout(() => {
+      setAnim("");
+    }, 5000);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <div className="App-header">
+        <img src={anim.length ? blade : circ} alt="circuit" className={anim} />
+        <h3 style={{ color: "black", fontFamily: "cursive" }}>
+          {" "}
+          t = 1.1 * R * C{" "}
+        </h3>
+        <button
+          onClick={() => {
+            IoT.emit("iotInit", 10);
+          }}
+          style={
+            anim.length
+              ? { background: "#de5246 ", cursor: "pointer" }
+              : { background: "rgb(19, 0, 71)", cursor: "pointer" }
+          }
+          disabled={anim.length ? true : false}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          {!anim.length ? "Send Signal" : <i>Oscillating...</i>}
+        </button>
+      </div>
+    </>
   );
 }
 
